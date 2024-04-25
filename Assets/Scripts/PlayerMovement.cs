90c138e67c5adfcb7e2ad6932383a11d1c77d3ba
reisuj <reisuj@gmail.com>
2024-04-25 17:53:43 -0400

    WIP: Early state of world rotation and player movement provides basic funtionality

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private bool enableDoubleJump = true;
 
     private Rigidbody2D rb;
+    private Collider2D coll;
     [SerializeField]
     private bool isGrounded;
     private bool canDoubleJump;
@@ -16,6 +17,7 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
     }
 
     void Update()
@@ -63,6 +65,35 @@ public class PlayerMovement : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = false;
+        }
+    }
+
+    public void EnablePlayerPhysics()
+    {
+        Debug.Log("Physics Enabled");
+        if (rb != null)
+        {
+            rb.gravityScale = 1.0f;
+        }
+
+        if (coll != null)
+        {
+            coll.enabled = true;
+        }
+    }
+
+    public void DisablePlayerPhysics()
+    {
+        Debug.Log("Physics Disabled");
+        if (rb != null)
+        {
+            rb.gravityScale = 0f;
+            rb.velocity = Vector2.zero;
+        }
+
+        if (coll != null)
+        {
+            coll.enabled = false;
         }
     }
 }
